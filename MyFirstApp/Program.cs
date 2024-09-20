97e3cbd4fb44ecf82ca9cfaf83778b25d3c155ca
Moks <robert.moks@tktk.ee>
2024-09-20 16:41:14 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string username = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + username + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {username} !"); //Output
